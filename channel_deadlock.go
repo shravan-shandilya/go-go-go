e32c859a6ca4b7a946e04bee8ed060514ecb623b
Shravan Shandilya <s.shravan95@gmail.com>
2017-05-18 06:47:25 +0530

    Update channel_deadlock.go

--- channel_deadlock.go
+++ channel_deadlock.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main(){
+  //Unbuffered channel, it means that channel cannot store any messages.
+  //Every message sent has to be immediately transfered to reciever
   ch := make(chan string)
   
   //Channel sending and recieving are blocking calls.
